659cca7687856392306bca952c54dcee8d07eae2
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-01-11 14:17:39 -0500

    Import project structure

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2020 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
