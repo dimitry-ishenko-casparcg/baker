e1dadc0de1645b6c3806b47ebf11ace209643587
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-01-13 18:56:12 -0500

    pie::device: major refactor
    Add toggle buttons.

--- pie/device.cpp
+++ pie/device.cpp
@@ -39,6 +39,7 @@ device::device(asio::io_context& io, const fs::path& path) :
     auto dd = data.as<descriptor_data>();
     columns_ = dd->columns;
     rows_ = dd->rows;
+    buttons_.resize(columns_ * CHAR_BIT);
 
     leds_on(fd_, leds::none);
 
@@ -48,6 +49,7 @@ device::device(asio::io_context& io, const fs::path& path) :
     level(fd_, 255, 255);
     period(fd_, 10);
 
+    request_data(fd_);
     sched_read();
 }
 
@@ -61,59 +63,134 @@ void device::sched_read()
 ////////////////////////////////////////////////////////////////////////////////
 void device::read_data(const asio::error_code& ec, std::size_t n)
 {
-    if(!ec)
+    if(ec) return;
+
+    if(n < sizeof(general_data)) throw std::runtime_error{
+        "Short read - general_data"
+    };
+    auto [ pressed, released ] = decode_buttons();
+
+    // handle PS separately as it's not part of buttons_
+    if(pressed.count(ps))
     {
-        if(n < sizeof(general_data)) throw std::runtime_error{
-            "Short read - general_data"
-        };
-        auto gd = data_.as<general_data>();
-
-        if(!gd->ps)
+        if(pressed_once_ != none)
         {
-            if(pressed_.count(ps))
-            {
-                release(ps);
-                toggle_locked();
-            }
+            un_blink(pressed_once_);
+            pressed_once_ = none;
         }
-        else if(!pressed_.count(ps)) press(ps);
-
-        auto [ pressed, released ] = decode_buttons(gd->buttons);
-
-        for(auto btn : pressed)
-        {
-            // reset pending double-press button
-            // if a different one was pressed
-            if(btn != pending_ && pending_ != none) un_pend();
-
-            if(btn == pending_ || !double_press_.count(btn))
-            {
-                // if this button is part of a group
-                if(auto gi = group_.find(btn); gi != group_.end())
-                {
-                    auto grp = gi->second;
-
-                    // find which button is currently active in this group
-                    // and release it
-                    if(auto bi = active_.find(grp); bi != active_.end()) release(bi->second);
-
-                    active_[grp] = btn;
-                }
-
-                press(btn);
-            }
-            else pend(btn);
-        }
-
-        for(auto btn : released)
-        {
-            // only release un-groupped buttons
-            // group buttons are released when another in the same group is pressed
-            if(group_.find(btn) == group_.end()) release(btn);
-        }
-
-        sched_read();
+        press(ps);
+        pressed.erase(ps);
     }
+    else if(released.count(ps))
+    {
+        release(ps);
+        released.erase(ps);
+
+        toggle_locked();
+    }
+
+    if(!locked_) for(auto idx : pressed)
+    {
+        auto& btn = buttons_[idx];
+        if(btn.double_press)
+        {
+            if(idx == pressed_once_) // 2nd press
+            {
+                pressed_once_ = none;
+
+                if(!pressed_.count(idx))
+                {
+                    if(btn.group) for(auto p_idx : pressed_)
+                        if(buttons_[p_idx].group == btn.group)
+                        {
+                            release(p_idx);
+                            break;
+                        }
+                    press(idx);
+                }
+                else if(btn.toggle) release(idx);
+            }
+            else // different button
+            {
+                if(pressed_once_ != none) un_blink(pressed_once_);
+
+                if(btn.toggle || !pressed_.count(idx))
+                {
+                    pressed_once_ = idx;
+                    blink(idx);
+                }
+            }
+        }
+        else // !btn.double_press
+        {
+            if(pressed_once_ != none)
+            {
+                un_blink(pressed_once_);
+                pressed_once_ = none;
+            }
+
+            if(!pressed_.count(idx))
+            {
+                if(btn.group) for(auto p_idx : pressed_)
+                    if(buttons_[p_idx].group == btn.group)
+                    {
+                        release(p_idx);
+                        break;
+                    }
+                press(idx);
+            }
+            else if(btn.toggle) release(idx);
+        }
+    }
+
+    for(auto idx : released)
+        if(idx != pressed_once_)
+        {
+            auto& btn = buttons_[idx];
+            // toggle and group buttons are released separately
+            if(!btn.toggle && !btn.group) release(idx);
+        }
+
+    sched_read();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+auto device::decode_buttons() -> std::tuple<indices, indices>
+{
+    indices pressed, released;
+
+    auto data = data_.as<general_data>();
+    auto prev = prev_.as<general_data>();
+
+    if(data->ps != prev->ps)
+    {
+        if(data->ps) pressed.insert(ps);
+        else released.insert(ps);
+
+        prev->ps = data->ps;
+    }
+
+    index idx = 0;
+    for(auto col = 0; col < columns_; ++col)
+    {
+        auto on =  data->buttons[col] & ~prev->buttons[col];
+        auto off= ~data->buttons[col] &  prev->buttons[col];
+        prev->buttons[col] = data->buttons[col];
+
+        for(auto row = 0; row < rows_; ++row)
+        {
+            if(on & 1) pressed.insert(idx);
+            on >>= 1;
+
+            if(off & 1) released.insert(idx);
+            off >>= 1;
+
+            ++idx;
+        }
+        idx += CHAR_BIT - rows_;
+    }
+
+    return { std::move(pressed), std::move(released) };
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -130,92 +207,62 @@ void device::toggle_locked()
         light_on(fd_, light::bank_1, all_rows);
         light_on(fd_, light::bank_2, no_rows);
 
-        // light up active group buttons
-        for(auto [_, btn] : active_)
-        {
-            light_state(fd_, columns_, btn, light::bank_1, off);
-            light_state(fd_, columns_, btn, light::bank_2, on);
-        }
+        for(auto idx : pressed_) activate(idx);
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-auto device::decode_buttons(byte buttons_[]) -> std::tuple<buttons, buttons>
+void device::blink(index idx)
 {
-    buttons pressed, released;
-
-    button b = 0;
-    for(auto col = 0; col < columns_; ++col)
-    {
-        auto on = buttons_[col];
-        for(auto row = 0; row < rows_; ++row)
-        {
-            if(on & 1)
-            {
-                // don't allow button presses when locked,
-                // only allow releases
-                if(!locked_ && !pressed_.count(b)) pressed.insert(b);
-            }
-            else if(pressed_.count(b)) released.insert(b);
-            on >>= 1;
-
-            ++b;
-        }
-        b += CHAR_BIT - rows_;
-    }
-
-    return { std::move(pressed), std::move(released) };
+    light_state(fd_, columns_, idx, light::bank_1, off);
+    light_state(fd_, columns_, idx, light::bank_2, flash);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void device::pend(button b)
+void device::un_blink(index idx)
 {
-    light_state(fd_, columns_, b, light::bank_1, off);
-    light_state(fd_, columns_, b, light::bank_2, flash);
-    pending_ = b;
+    if(pressed_.count(idx))
+        activate(idx);
+    else deactivate(idx);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void device::un_pend()
+void device::activate(index idx)
 {
-    light_state(fd_, columns_, pending_, light::bank_1, on);
-    light_state(fd_, columns_, pending_, light::bank_2, off);
-    pending_ = none;
+    light_state(fd_, columns_, idx, light::bank_1, off);
+    light_state(fd_, columns_, idx, light::bank_2, on);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void device::press(button btn)
+void device::deactivate(index idx)
 {
-    if(btn != ps)
-    {
-        light_state(fd_, columns_, btn, light::bank_1, off);
-        light_state(fd_, columns_, btn, light::bank_2, on);
-    }
+    light_state(fd_, columns_, idx, light::bank_1, on);
+    light_state(fd_, columns_, idx, light::bank_2, off);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void device::press(index idx)
+{
+    if(idx != ps)
+        activate(idx);
     else led_state(fd_, led::red, on);
 
-    pressed_.insert(btn);
-    pending_ = none;
-
-    if(pcall_) pcall_(btn);
+    pressed_.insert(idx);
+    if(pcall_) pcall_(idx);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-void device::release(button btn)
+void device::release(index idx)
 {
-    if(btn != ps)
+    if(idx != ps)
     {
         // when locked, leave the button red (bank_2)
-        if(!locked_)
-        {
-            light_state(fd_, columns_, btn, light::bank_1, on);
-            light_state(fd_, columns_, btn, light::bank_2, off);
-        }
+        if(!locked_) deactivate(idx);
     }
     else led_state(fd_, led::red, off);
 
-    pressed_.erase(btn);
-
-    if(rcall_) rcall_(btn);
+    pressed_.erase(idx);
+    if(rcall_) rcall_(idx);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- pie/device.hpp
+++ pie/device.hpp
@@ -15,9 +15,10 @@
 #include <filesystem>
 #include <functional>
 #include <initializer_list>
-#include <map>
+#include <optional>
 #include <set>
 #include <tuple>
+#include <vector>
 
 namespace fs = std::filesystem;
 
@@ -26,9 +27,8 @@ namespace pie
 {
 
 ////////////////////////////////////////////////////////////////////////////////
-using button_list = std::initializer_list<button>;
-
-using callback = std::function<void (button)>;
+using index_list = std::initializer_list<index>;
+using callback = std::function<void (index)>;
 
 ////////////////////////////////////////////////////////////////////////////////
 class device
@@ -37,12 +37,16 @@ public:
     explicit device(asio::io_context&, const fs::path&);
 
     // mark button(s) as double-press
-    void double_press(button b) { double_press_.insert(b); }
-    void double_press(button_list l) { for(auto b : l) double_press(b); }
+    void double_press(index idx) { buttons_.at(idx).double_press = true; }
+    void double_press(index_list il) { for(auto idx : il) double_press(idx); }
+
+    // mark button(s) as toggle
+    void toggle(index idx) { buttons_.at(idx).toggle = true; }
+    void toggle(index_list il) { for(auto idx : il) toggle(idx); }
 
     // add button(s) to a group
-    void group(button b, int id) { group_[b] = id; }
-    void group(button_list l, int id) { for(auto b : l) group(b, id); }
+    void group(index idx, int id) { buttons_.at(idx).group = id; }
+    void group(index_list il, int id) { for(auto idx : il) group(idx, id); }
 
     void pressed_callback(callback cb) { pcall_ = std::move(cb); }
     void released_callback(callback cb) { rcall_ = std::move(cb); }
@@ -51,33 +55,37 @@ private:
     fd fd_;
     byte columns_, rows_;
 
-    recv data_;
+    struct button
+    {
+        bool double_press = false;
+        bool toggle = false;
+        std::optional<int> group;
+    };
+    std::vector<button> buttons_;
+
+    callback pcall_, rcall_;
+
+    recv data_ { }, prev_ { };
     void sched_read();
 
-    void read_data(const asio::error_code&, std::size_t n);
+    using indices = std::set<index>;
 
-    using buttons = std::set<button>;
+    void read_data(const asio::error_code&, std::size_t n);
+    std::tuple<indices, indices> decode_buttons();
+
+    index pressed_once_ = none;
+    indices pressed_;
+
+    void blink(index);
+    void un_blink(index);
+    void activate(index);
+    void deactivate(index);
+
+    void press(index);
+    void release(index);
 
     bool locked_ = false;
     void toggle_locked();
-
-    buttons double_press_;
-    button pending_ = none; // pending double-press button (was pressed once)
-
-    std::map<button, int> group_;
-    std::map<int, button> active_;
-
-    buttons pressed_;
-
-    std::tuple<buttons, buttons> decode_buttons(byte[]);
-
-    void pend(button);
-    void un_pend();
-
-    void press(button);
-    void release(button);
-
-    callback pcall_, rcall_;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
