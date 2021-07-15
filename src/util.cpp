3b306baa6bfe13e627f3fcc56fdd590229c0f55d
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2021-07-14 22:35:23 -0400

    util: update
    Remove data_path().
    set_interrupt_callback -> on_interrupt.

--- src/util.cpp
+++ src/util.cpp
@@ -1,21 +1,19 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2020 Dimitry Ishenko
+// Copyright (c) 2020-2021 Dimitry Ishenko
 // Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
 //
 // Distributed under the GNU GPL license. See the LICENSE.md file for details.
 
 ////////////////////////////////////////////////////////////////////////////////
 #include "util.hpp"
-
 #include <csignal>
-#include <cstdlib>
 
 ////////////////////////////////////////////////////////////////////////////////
 namespace src
 {
 
 ////////////////////////////////////////////////////////////////////////////////
-void set_interrupt_callback(interrupt_callback cb)
+void on_interrupt(interrupt_callback cb)
 {
     static interrupt_callback cb_;
 
@@ -26,21 +24,12 @@ void set_interrupt_callback(interrupt_callback cb)
     }
 
     cb_ = std::move(cb);
-    if(!cb_) std::signal(SIGINT, SIG_DFL), std::signal(SIGTERM, SIG_DFL);
-}
 
-////////////////////////////////////////////////////////////////////////////////
-fs::path data_path()
-{
-#if defined(_WIN32)
-    return fs::path{ std::getenv("APPDATA") };
-#elif defined(__APPLE__)
-    return fs::path{ std::getenv("HOME") } / "Library" / "Application Support";
-#elif defined(__unix__)
-    return fs::path{ std::getenv("HOME") } / ".local" / "share";
-#else
-    #error "Unsupported platform"
-#endif
+    if(!cb_)
+    {
+        std::signal(SIGINT, SIG_DFL);
+        std::signal(SIGTERM, SIG_DFL);
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
